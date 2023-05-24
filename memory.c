ebe042f39e3ea393b9e2124c00104f7e7ad5f1d7
Adomako George Junior <georgeadomakojnr51@aol.com>
2023-05-24 00:53:17 +0000

    adding fist part of codes

---
+++ memory.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+
+/**
+ * bfree - to free a pointer and NULLs the address
+ * @ptr: address of the pointer to be freed
+ * Return: 1 if frees, otherwise 0
+ */
+int bfree(void **ptr)
+{
+	if (ptr && *ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
+	}
+	return (0);
+}
